8a04ef2e39f2e24851b9ed88464e0b4233408f96
porrtal <98087653+porrtal@users.noreply.github.com>
2022-01-27 03:22:56 -0700

    test

--- lib/msal-angular/src/constants.ts
+++ lib/msal-angular/src/constants.ts
@@ -4,7 +4,7 @@
  */
 
 import { InjectionToken } from "@angular/core";
-
+  
 export const MSAL_INSTANCE = new InjectionToken<string>("MSAL_INSTANCE");
 
 export const MSAL_GUARD_CONFIG = new InjectionToken<string>("MSAL_GUARD_CONFIG");
